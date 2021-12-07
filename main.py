f1774bb5886c3d1216eced6589168de90da78716
snorresovold <snorresovold@gmail.com>
2021-12-07 11:59:57 +0100

    it works

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import numpy as np
 import cv2
+import numpy as np
 
-
-
-# check out for more info https://stackoverflow.com/questions/66718462/python-cv-detect-different-types-of-arrows
 def preprocess(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
@@ -13,13 +10,32 @@ def preprocess(img):
     img_erode = cv2.erode(img_dilate, kernel, iterations=1)
     return img_erode
 
-
 def find_tip(points, convex_hull):
     length = len(points)
     indices = np.setdiff1d(range(length), convex_hull)
+
     for i in range(2):
         j = indices[i] + 2
         if j > length - 1:
             j = length - j
-            if np.all(points[j] == points[indices[i - 1] - 2]):
-                return tuple(points[j])
+        if np.all(points[j] == points[indices[i - 1] - 2]):
+            return tuple(points[j])
+
+img = cv2.imread("FHtLk.png")
+
+contours, hierarchy = cv2.findContours(preprocess(img), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+for cnt in contours:
+    peri = cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, 0.025 * peri, True)
+    hull = cv2.convexHull(approx, returnPoints=False)
+    sides = len(hull)
+
+    if 6 > sides > 3 and sides + 2 == len(approx):
+        arrow_tip = find_tip(approx[:,0,:], hull.squeeze())
+        if arrow_tip:
+            cv2.drawContours(img, [cnt], -1, (0, 255, 0), 3)
+            cv2.circle(img, arrow_tip, 3, (0, 0, 255), cv2.FILLED)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
