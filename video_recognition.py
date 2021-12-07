4ea76cf0e9d1636cf1d0b43d064d96f14c562b6d
snorresovold <snorresovold@gmail.com>
2021-12-07 12:14:14 +0100

    works on images

---
+++ video_recognition.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+def preprocess(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
+    img_canny = cv2.Canny(img_blur, 50, 50)
+    kernel = np.ones((3, 3))
+    img_dilate = cv2.dilate(img_canny, kernel, iterations=2)
+    img_erode = cv2.erode(img_dilate, kernel, iterations=1)
+    return img_erode
+
+def find_tip(points, convex_hull):
+    length = len(points)
+    indices = np.setdiff1d(range(length), convex_hull)
+
+    for i in range(2):
+        j = indices[i] + 2
+        if j > length - 1:
+            j = length - j
+        if np.all(points[j] == points[indices[i - 1] - 2]):
+            return tuple(points[j])
+
+
