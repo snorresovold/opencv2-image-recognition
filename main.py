69db7fd0738213ed292e1d00bf44e489209b15f0
snorresovold <snorresovold@gmail.com>
2021-12-07 10:40:33 +0100

    added an arrow image

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import numpy as np
 import cv2
 
-img = cv2.imread('arrow.png', 0)
-template = cv2.imread('arrow.png', 0)
+img = cv2.resize(cv2.imread('arrow.webp', 0), (0, 0), fx=0.8, fy=0.8)
+template = cv2.resize(cv2.imread('arrow.webp', 0), (0, 0), fx=0.8, fy=0.8)
 h, w = template.shape
 
-methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
-            cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
+#methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
 
-for method in methods:
-    img2 = img.copy()
-    result = cv2.matchTemplate(img2, template, method)
+method = cv2.TM_CCOEFF
 
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    print(min_loc, max_loc)
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        location = min_loc
-    else:
-        location = max_loc
+
+
+img2 = img.copy()
+
+result = cv2.matchTemplate(img2, template, method)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+    location = min_loc
+else:
+    location = max_loc
+
+bottom_right = (location[0] + w, location[1] + h)    
+cv2.rectangle(img2, location, bottom_right, 255, 5)
+cv2.imshow('Match', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
